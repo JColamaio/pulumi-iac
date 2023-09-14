7fb24fa84132e6beacacfcaf422a10ca5069d1a8
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-09-14 10:43:50 -0300

    instance EC2

--- code/__main__.py
+++ code/__main__.py
@@ -1,10 +1,16 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
-
+from pulumi_aws import s3, ec2
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket('my-bucket')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
+
+# Create an AWS resource (EC2 Instance)
+ec2_instance = ec2.Instance('web-server',ami="ami-053b0d53c279acc90", instance_type="t3.nano", key_name='test', tags ={
+    "Name": "web"
+})
+
+pulumi.export('public_ip', ec2_instance.public_ip)
